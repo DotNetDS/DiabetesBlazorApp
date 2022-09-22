76c043f378292d0d735ffa1d3318228901be0368
David Skilbeck <david.skilbeck@oranasoftware.com>
2022-09-23 07:45:17 +1000

    Add project files.

---
+++ Server/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DiabetesBlazorApp.Shared;
+
+namespace DiabetesBlazorApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        List<GlucoseValue> glucoseValues = new List<GlucoseValue>
+        {
+            new GlucoseValue
+            {
+                FirstName = "Peter", LastName = "Parker", Value = "6.7" },
+            
+            new GlucoseValue
+            {
+                FirstName = "Derick", LastName = "Jones", Value = "7.1"}
+        };
+
+
+        public async Task<IActionResult> GetGlucoseValues()
+        {
+            return Ok(glucoseValues);
+        }
+    }
+}
